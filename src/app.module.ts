db6aadbb470cd6df7d3b8d6960b450443721e656
Olena Ryndina <olenaryndina1111@gmail.com>
2022-10-06 16:27:21 +0300

    entity, TypOrmModule, TodoModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { TodoModule } from './modules/todo/todo.module';
+import { Todo } from './modules/todo/entities/todo.entity';
 
 @Module({
-  imports: [],
+  imports: [
+      TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'todo',
+      entities: [Todo],
+      synchronize: true,
+    }),
+      TodoModule
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+      AppService
+  ],
+  exports: [
+  ],
 })
 export class AppModule {}
