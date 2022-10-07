b571c3093b4a070eee975b669eb22e199cf42dfd
Olena Ryndina <olenaryndina1111@gmail.com>
2022-10-07 11:01:27 +0300

    CRUD operations

--- src/modules/todo/controllers/todo.controller.ts
+++ src/modules/todo/controllers/todo.controller.ts
@@ -1,40 +1,56 @@
-import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body, NotFoundException, HttpException, HttpStatus } from '@nestjs/common';
 
 import { Todo } from '../entities/todo.entity';
 import { CreateDto, UpdateDto } from './dto';
+import { TodoService } from '../services/todo.service';
 
 @Controller('rest/todo')
 export class TodoController {
-    constructor() {}
+    constructor(
+        private readonly todoService: TodoService
+    ) {}
 
     @Get()
-    getAllAction(): string {
-        return "get all";
+    getAllAction(): Promise<Todo[]> {
+        return this.todoService.findAll();
     }
 
     @Get(':id')
-    getOneAction(@Param('id') id: string): string {
-        return "get One by id: " + id;
+    async getOneAction(@Param('id') id: string): Promise<Todo> {
+        const todo = this.todoService.findOne(id);
+        if (todo === undefined) {
+            throw new HttpException('Not found', HttpStatus.NOT_FOUND);
+        }
+        return todo;
     }
 
     @Post()
-    createAction(@Body() todo: CreateDto): CreateDto {
-        console.log(todo);
-        return todo;
+    createAction(@Body() createDto: CreateDto): Promise<Todo> {
+        const todo = new Todo();
+        todo.title = createDto.title;
+
+        if (createDto.isCompleted !== undefined) {
+            todo.isCompleted = createDto.isCompleted;
+        }
+        return this.todoService.create(todo);
     }
 
-    @Put('id')
-    updateAction(
+    @Put(':id')
+    async updateAction(
         @Param('id') id: string, 
-        @Body() todo: UpdateDto
-    ): UpdateDto {
-        console.log('search by ID: ', id);
-        console.log(todo, 'saved');
-        return todo;
+        @Body() {title, isCompleted = false}: UpdateDto
+    ): Promise<Todo> {
+        const todo = await this.todoService.findOne(id);
+        if (todo === undefined) {
+            throw new NotFoundException('Todo # ' + id + ' does not exist');
+        }
+        todo.title = title;
+        todo.isCompleted = isCompleted;
+        return this.todoService.update(todo);
     }
 
     @Delete(':id')
-        deleteAction(@Param('id') id: string): string {
-        return "delete id: " + id;
+        deleteAction(@Param('id') id: string): Promise<void> {
+        return this.todoService.remove(id);
     }
 }
--- src/modules/todo/todo.module.ts
+++ src/modules/todo/todo.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { Todo } from './entities/todo.entity';
 import { TodoController } from './controllers/todo.controller';
+import { TodoService } from './services/todo.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Todo])],
   controllers: [TodoController],
-  providers: [],
+  providers: [TodoService],
 })
 export class TodoModule {}
